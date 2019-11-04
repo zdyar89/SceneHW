9da10f0563b15af7470c07ffad8ced9760366616
Taylor <rvd651@mocs.utc.edu>
2019-11-04 14:26:39 -0500

    Main menu template finished

--- src/MainGame.java
+++ src/MainGame.java
@@ -12,6 +12,12 @@ public class MainGame extends Game implements Scene {
 
     public static void main(String[] args) {
         MainGame game = new MainGame();
+        game.registerGlobalCallbacks();
+        SimpleMenu mainMenu = new SimpleMenu();
+        mainMenu.addItem(new SimpleMenu.SelectableText(20, 20, 20, 20, "Launch Game", 1, 0, 0, 1, 1, 1), game);
+        mainMenu.addItem(new SimpleMenu.SelectableText(20, 60, 20, 20, "Exit", 1, 0, 0, 1, 1, 1), null);
+        mainMenu.select(0);
+        game.setScene(mainMenu);
         game.gameLoop();
     }
 
